654f6a552ef0eb153fbb91b021aecc1e2896bc79
Martynas Janušaitis <martynasjanusaitis@proton.me>
2024-05-11 00:41:50 +0300

    Added msl_algorithm.h

---
+++ msl_algorithm.h
@@ -0,0 +1,31 @@
+#ifndef MSL_ALGORITHM_H
+#define MSL_ALGORITHM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void msl_swap(void *a, void *b, size_t s)
+{
+    void *temp = malloc(s);
+    memcpy(temp, a, s);
+    memcpy(a, b, s);
+    memcpy(b, temp, s);
+    free(temp);
+}
+
+static void *msl_find(void *begin, void *end, size_t s, void *val)
+{
+    void *buffer = malloc(s);
+    void *p;
+
+    for(p = begin; p < end; p = (char*)p + s)
+    {
+        if(strncmp((char*)val, (char*)p, s) == 0) break;        
+    }
+
+    free(buffer);
+    return p;
+}
+
+#endif
