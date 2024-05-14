02e9e74c034cb4a05dfdabb6bae69517988369ec
Martynas Janušaitis <martynasjanusaitis@proton.me>
2024-05-14 22:09:15 +0300

    Added function declarations to msl_vector.h

--- msl_vector.h
+++ msl_vector.h
@@ -13,6 +13,19 @@ typedef struct msl_vector3
     float x, y, z; 
 } msl_vector3;
 
+
+static msl_vector2 msl_vector2_add(msl_vector2 a, msl_vector2 b);
+static msl_vector2 msl_vector2_sub(msl_vector2 a, msl_vector2 b);
+static msl_vector2 msl_vector2_scalar_product(msl_vector2 a, float f);
+static float msl_vector2_dot_product(msl_vector2 a, msl_vector2 b);
+static float msl_vector2_magnitude(msl_vector2 a);
+
+static msl_vector3 msl_vector3_add(msl_vector3 a, msl_vector3 b);
+static msl_vector3 msl_vector3_sub(msl_vector3 a, msl_vector3 b);
+static msl_vector3 msl_vector3_cross_product(msl_vector3 a, msl_vector3 b);
+static float msl_vector3_dot_product(msl_vector3 a, msl_vector3 b);
+static float msl_vector3_magnitude(msl_vector3 a);
+
 static msl_vector2 msl_vector2_add(msl_vector2 a, msl_vector2 b)
 {
     return (msl_vector2){a.x + b.x, a.y + b.y};
