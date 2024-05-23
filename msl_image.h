ace2a52f314d1412d44f2a4fb8713cb1f0c5c241
Martynas Janušaitis <martynasjanusaitis@proton.me>
2024-05-23 10:22:50 +0300

    Total reworking of how BMP images are opened in msl_image.h

--- msl_image.h
+++ msl_image.h
@@ -1,12 +1,14 @@
 #ifndef MSL_IMAGE_H
 #define MSL_IMAGE_H
 
+#include <stdint.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef unsigned int msl_ui;
-typedef unsigned short msl_us;
-typedef unsigned char msl_uc;
+typedef uint32_t msl_ui;
+typedef uint16_t msl_us;
+typedef uint8_t msl_uc;
 
 enum msl_image_format{
     BMP,
@@ -20,19 +22,21 @@ typedef struct msl_image
 {
     enum msl_image_format format;
     msl_ui img_x, img_y;
+    msl_uc bits_per_pixel;
     msl_uc *data, *data_end;
     msl_uc *buffer, *buffer_end;
 } msl_image;
+static msl_ui msl_read_32_le(msl_uc **buff);
+static msl_us msl_read_16_le(msl_uc **buff);
+static msl_ui msl_read_32_be(msl_uc **buff);
+static msl_us msl_read_16_be(msl_uc **buff);
+static msl_uc msl_read_8(msl_uc **buff);
 
-static msl_ui msl_read_32_le(msl_uc *buff, size_t pos);
-static msl_us msl_read_16_le(msl_uc *buff, size_t pos);
-static msl_ui msl_read_32_be(msl_uc *buff, size_t pos);
-static msl_us msl_read_16_be(msl_uc *buff, size_t pos);
-static msl_uc msl_read_8(msl_uc *buff, size_t pos);
 
 static int msl_image_read_from_file(FILE *f, msl_image *image);
 static int msl_image_read(char *filename, msl_image *image);
 static int msl_image_parse(msl_image *image);
+static void msl_image_free(msl_image *image);
 
 static enum msl_image_format msl_image_check_header(msl_image *image);
 static int msl_image_check_header_bmp(msl_image *image);
@@ -43,25 +47,27 @@ static int msl_image_check_header_gif(msl_image *image);
 static int msl_image_load_bmp(msl_image *image);
 static int msl_image_load_png(msl_image *image);
 
-static msl_ui msl_read_32_le(msl_uc *buff, size_t pos)
+static msl_ui msl_read_32_le(msl_uc **buff)
 {
-    return msl_read_16_le(buff, pos + 2) << 16 | msl_read_16_le(buff, pos);
+    return msl_read_16_le(buff) | msl_read_16_le(buff) << 16;
 }
-static msl_us msl_read_16_le(msl_uc *buff, size_t pos)
+static msl_us msl_read_16_le(msl_uc **buff)
 {
-    return msl_read_8(buff, pos + 1) << 8 | msl_read_8(buff, pos);
+    return msl_read_8(buff) | msl_read_8(buff) << 8;
 }
-static msl_ui msl_read_32_be(msl_uc *buff, size_t pos)
+static msl_ui msl_read_32_be(msl_uc **buff)
 {
-    return msl_read_16_be(buff, pos) << 16 | msl_read_16_be(buff, pos + 2);
+    return msl_read_16_be(buff) << 16 | msl_read_16_be(buff);
 }
-static msl_us msl_read_16_be(msl_uc *buff, size_t pos)
+static msl_us msl_read_16_be(msl_uc **buff)
 {
-    return msl_read_8(buff, pos) << 8 | msl_read_8(buff, pos + 1);
+    return msl_read_8(buff) << 8 | msl_read_8(buff);
 }
-static msl_uc msl_read_8(msl_uc *buff, size_t pos)
+static msl_uc msl_read_8(msl_uc **buff)
 {
-    return *(buff + pos);
+    msl_uc ret = **(buff);
+    (*buff)++;
+    return ret;
 }
 
 
@@ -73,7 +79,11 @@ static int msl_image_read(char *filename,  msl_image *image)
         printf("Failed to open file\n");
         return -1;
     }
-    msl_image_read_from_file(f, image);
+    if (msl_image_read_from_file(f, image) != 0)
+    {
+        fclose(f);
+        return -1;
+    }
     fclose(f);
     return 0;
 }
@@ -100,7 +110,6 @@ static int msl_image_parse(msl_image *image)
     switch(msl_image_check_header(image))
     {
         case BMP :
-            printf("The file is a BMP\n");
             return msl_image_load_bmp(image);
         case PNG :
             printf("The file is a PNG\n");
@@ -120,6 +129,11 @@ static int msl_image_parse(msl_image *image)
 
     return -1;
 }
+static void msl_image_free(msl_image *image)
+{
+    free(image->data);
+    free(image->buffer);
+}
 
 static enum msl_image_format msl_image_check_header(msl_image *image)
 {
@@ -137,12 +151,62 @@ static int msl_image_check_header_bmp(msl_image *image)
 
 static int msl_image_load_bmp(msl_image *image)
 {
-    if(msl_image_check_header_bmp(image) != 0) return -1;
     msl_uc *buff = image->buffer;
-    msl_ui offset = msl_read_32_le(buff, 10);
+    
+    // BMP file header
+    if(msl_read_8(&buff) != 0x42) return -1; // BMP identifier B
+    if(msl_read_8(&buff) != 0x4D) return -1; // BMP identifier M
+    if(msl_read_32_le(&buff) == 0) return -1; // BMP image is zero bytes
+    msl_read_16_le(&buff); // Reserved bytes
+    msl_read_16_le(&buff); // Reserved bytes
+    msl_ui offset = msl_read_32_le(&buff); // Pixel data offset
 
-    image->data = image->buffer + offset;
-    image->data_end = image->buffer_end;
+    // Windows BITMAPINFOHEADER
+    msl_ui header_size = msl_read_32_le(&buff);
+    image->img_x = msl_read_32_le(&buff);
+    image->img_y = msl_read_32_le(&buff);
+    msl_ui color_planes = msl_read_16_le(&buff);
+    if(color_planes != 1) // Number of color planes must be 1 
+    {
+        printf("BMP file might be corrupted\n");
+        printf("The number of color planes was: %x\n", color_planes);
+        return -1;
+    }     
+    image->bits_per_pixel = msl_read_16_le(&buff);
+    if(image->bits_per_pixel != 24)
+    {
+        printf("Only 24 bits per pixel are supported\n");
+        printf("Currently bits per pixel: %d\n", image->bits_per_pixel);
+        return -1;
+    }
+    if(msl_read_32_le(&buff) != 0)
+    {
+        printf("BMP image compression method unsupported\n");
+        return -1;
+    }
+    msl_read_32_le(&buff); // Raw bitmap data size
+    msl_read_32_le(&buff); // Horizontal resolution
+    msl_read_32_le(&buff); // Vertical resolution
+    msl_read_32_le(&buff); // Colors in the color palette
+    msl_read_32_le(&buff); // Number of important colors
+
+    
+    image->data = (msl_uc*) malloc(image->img_x * image->img_y * 3);
+    image->data_end = image->data + (image->img_x * image->img_y * 3);
+
+    buff = image->buffer + offset;
+    msl_ui i = 0;
+    for(buff = image->buffer + offset; buff < image->buffer_end;)
+    {
+        image->data[i + 2] = msl_read_8(&buff); // Blue
+        image->data[i + 1] = msl_read_8(&buff); // Green
+        image->data[i] = msl_read_8(&buff); // Red 
+        
+        i += 3;
+        if(i % 15 == 0) buff++;
+    }
+
+    image->format = BMP;
 
     return 0;
 }
