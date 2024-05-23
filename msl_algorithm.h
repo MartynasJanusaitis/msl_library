351217cb238240b9591b2f6a6a56369bf0a51783
Martynas Janušaitis <martynasjanusaitis@proton.me>
2024-05-23 10:20:15 +0300

    Added map function to msl_algorithm.h

--- msl_algorithm.h
+++ msl_algorithm.h
@@ -1,6 +1,7 @@
 #ifndef MSL_ALGORITHM_H
 #define MSL_ALGORITHM_H
 
+#include <cstdint>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +9,8 @@
 
 static void msl_swap(void *a, void *b, size_t s);
 static void *msl_find(void *begin, void *end, size_t s, void *val);
+static uint32_t msl_map(int32_t val, int32_t from_low, int32_t from_high, int32_t to_low, int32_t to_high);
+static uint64_t msl_map_long(int64_t val, int64_t from_low, int64_t from_high, int64_t to_low, int64_t to_high);
 
 static void msl_swap(void *a, void *b, size_t s)
 {
@@ -26,6 +29,16 @@ static void *msl_find(void *begin, void *end, size_t s, void *val)
         if(strncmp((char*)val, (char*)p, s) == 0) break;        
     }
     return p;
+}
+
+static uint32_t msl_map(int32_t val, int32_t from_low, int32_t from_high, int32_t to_low, int32_t to_high)
+{
+    return (val - from_low) * (to_high - to_low) / (from_high - from_low) + to_low; 
+}
+
+static uint64_t msl_map_long(int64_t val, int64_t from_low, int64_t from_high, int64_t to_low, int64_t to_high)
+{
+    return (val - from_low) * (to_high - to_low) / (from_high - from_low) + to_low; 
 }
 
 #endif
